7a679a8f8bd4134f7a3bb47e63e97cdea80477d4
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-12-02 19:07:44 +0100

    Added Day 2 challenge

---
+++ Day 2 - Deposit Profit/script.js
@@ -0,0 +1,16 @@
+function depositProfit(deposit, rate, threshold) {
+    rate = rate / 100;
+    let numbOfYears = 0;
+    let balance = deposit;
+    
+    while (balance <= threshold) {
+        balance += balance * rate;
+        numbOfYears++;
+    }    
+    
+    return numbOfYears;
+}
+
+console.log('Years to reach threshold: ', depositProfit(100, 20, 170));
+console.log('Years to reach threshold: ', depositProfit(200, 20, 470));
+console.log('Years to reach threshold: ', depositProfit(10, 60, 180));
